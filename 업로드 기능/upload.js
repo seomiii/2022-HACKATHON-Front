ec92192ab26662034abff952e2c9d0a1904537c5
biyamn <biyamn@gmail.com>
2022-08-12 00:16:14 +0900

    캘린더, 이미지업로드 구현

--- 업로드 기능/upload.js
+++ 업로드 기능/upload.js
@@ -1,18 +1,6 @@
-let num = 1;
-function calendar() {
-  let calendar = document.querySelector(".calendar");
-  if (num % 2 === 1) {
-  $( function() {
-    $( "#datepicker" ).datepicker();
-  } );
-  num++;
-  console.log(num);
-  }
-  else {
-    $( "#datepicker" ).datepicker( "destroy" );
-    $( "#datepicker" ).hide();
-    $( "#datepicker" ).show();
-    num++;
-    console.log(num);
-  }
-}
+$(document).ready(function(){
+  // 실행할 기능을 정의해주세요.
+  $( "#datepicker" ).datepicker({
+    dateFormat: 'yy-mm-dd'
+  });
+});
