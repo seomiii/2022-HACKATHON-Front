86e5bf1d57c2cf1408db7e0d4b8dfea4897cab93
harim061 <doris5093@duksung.ac.kr>
2022-08-15 20:27:42 +0900

    날짜 드롭

---
+++ front/마이페이지/mypage.js
@@ -0,0 +1,7 @@
+const button1 = document.querySelector('.view-p');
+
+button1.addEventListener('click', () => {
+    const dropyear = document.querySelector('.dropyear');
+    dropyear.classList.toggle('dis2');
+});
+
